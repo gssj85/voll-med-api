3969ec3a977ba37a36db4dfe1f648144a7c0d510
Guilherme Souza <guilherme.souza@datum.inf.br>
2023-01-05 00:04:05 -0300

    Implementando atualização do Médico e enviando Ids das listagens.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -31,5 +28,12 @@ public class MedicoController {
             Pageable paginacao
     ) {
         return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dadosAtualizacaoMedico) {
+        var medico = medicoRepository.getReferenceById(dadosAtualizacaoMedico.id());
+        medico.atualizarInformacoes(dadosAtualizacaoMedico);
     }
 }
